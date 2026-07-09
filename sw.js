a130add314c6792de7d2d4e958b8faee0457598b
Travagliati <ltravagliati@libero.it>
2026-07-09 15:18:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE="phollowme-v1";
+const CACHE="phollowme-v2";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./icon-180.png"];
 self.addEventListener("install",function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}).then(function(){return self.skipWaiting();}));});
-self.addEventListener("activate",function(e){e.waitUntil(self.clients.claim());});
+self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
 self.addEventListener("fetch",function(e){
   if(e.request.method!=="GET"){return;}
   e.respondWith(caches.match(e.request).then(function(r){
