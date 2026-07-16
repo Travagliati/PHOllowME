747a81b3c5187558393dda058c4adfbe24f0727d
Travagliati <ltravagliati@libero.it>
2026-07-16 17:15:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE="phollowme-v4";
+const CACHE="phollowme-v5";
 const ASSETS=["./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./icon-180.png"];
 self.addEventListener("install",function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}).then(function(){return self.skipWaiting();}));});
 self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
 self.addEventListener("fetch",function(e){
   if(e.request.method!=="GET"){return;}
+  // ignora tutto cio' che non e' del nostro sito (Firebase, Google, ecc.): deve passare diretto dalla rete
+  try{ if(new URL(e.request.url).origin!==self.location.origin){ return; } }catch(err){ return; }
   var accept=e.request.headers.get("accept")||"";
   var isNav = e.request.mode==="navigate" || accept.indexOf("text/html")!==-1;
   if(isNav){
